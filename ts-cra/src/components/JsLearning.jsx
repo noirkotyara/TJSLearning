e860b1f8f6f9d579ca676205cc0d4b88919c9d82
noirkotyara <noirkotyara@gmail.com>
2021-02-15 22:23:21 +0200

    class/prototype/this

--- ts-cra/src/components/JsLearning.jsx
+++ ts-cra/src/components/JsLearning.jsx
@@ -1,3 +1,4 @@
+import { allowedNodeEnvironmentFlags } from "process"
 import { applyMiddleware, bindActionCreators } from "redux"
 
 let array3 = [1, 2, 3, 4, 5]
@@ -93,22 +94,83 @@ export const JsLearning = () => {
 
 ///bind
 
-let person = function () {
-    console.log(this)
-    console.log(`it is ${this.name} ${this.id} ${this.age} ${this.hello}`)
-}
+// let person = function () {
+//     console.log(this)
+//     console.log(`it is ${this.name} ${this.id} ${this.age} ${this.hello}`)
+// }
 
-const person1 = { id: 1, name: 'lena', age: 22, hello: 'fgfgf' }
-const person2 = { id: 2, name: 'Ivan', age: 18 }
+// const person1 = { id: 1, name: 'lena', age: 22, hello: 'fgfgf' }
+// const person2 = { id: 2, name: 'Ivan', age: 18 }
 
-function bind(fn, obj) {
-    return () => {
-        console.log(arguments)
-        fn.apply(obj, arguments)
-    }
-}
+// function bind(fn, obj) {
+//     return () => {
+//         console.log(arguments)
+//         fn.apply(obj, arguments)
+//     }
+// }
 
 // let func1 = bind(person, person1)
-bind(person, person1)()
+// bind(person, person1)()
 // console.log(bind.arguments())
 // bind(person ,person2)()
+
+//13.02.2021
+
+// let dog = {
+//     breed: 'Beagles',
+//     lovesToChase: 'rabbits'
+// }
+
+// function chase() {
+//     console.log(this.breed + ' loves chasing ' + this.lovesToChase )
+// }
+
+// dog.foo = chase
+// dog.foo()
+// chase() //error
+
+// function Yo(name) {
+//     this.name = name
+//     return this;
+// }
+
+// Yo.prototype.hello = function () {
+//     alert(this.name)
+// }
+
+// var yo1 = new Yo('dima')
+// yo1.__proto__
+// var yo2 = new Yo('dianna')
+// yo2.hello()
+
+//самовызывающайся функция
+
+// var func = (function(){
+//     // переменные не выходят отсюда
+//     var city = 'Minsk'
+//     var a = 3 // записівается в lexical environment
+//     return function(){
+//         alert(a)
+//     }
+// })()
+
+
+// var a = {
+//     name: 'Dima',
+//     sayName: function () {
+//         alert(this.name)
+//     }
+// }
+
+// var b = {
+//     name: 'Sveta'
+// }
+
+// b.sayName = a.sayName
+// b.sayName() // Dima
+
+// var c = a.sayName.bind(a)
+// c()//Dima
+
+// window.setTimeout(a.sayName, 1000) //this --> window
+
